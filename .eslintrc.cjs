653122a2a637b71e39b9dfc232eab3211cefc598
Thomas Queste <tom@tomsquest.com>
2023-09-25 18:39:42 +0200

    style: enable no-unused-imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,18 +8,35 @@ module.exports = {
     // Turns off all rules that are unnecessary or might conflict with Prettier.
     "prettier",
   ],
-  plugins: ["@typescript-eslint", "simple-import-sort", "import"],
+  plugins: [
+    "@typescript-eslint",
+    "simple-import-sort",
+    "import",
+    "unused-imports",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
   },
   ignorePatterns: ["/dist"],
   rules: {
-    /* Simple-import, Import */
+    /* Sort imports */
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
+    /* Make unused imports fixable */
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
 };
