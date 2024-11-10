6cb433932ed8d046d4b06c93eb79ac35f8342ce8
Thomas Queste <tom@tomsquest.com>
2024-11-10 08:06:36 +0100

    feat: get first command and print errors and usage

--- src/getHelloWorld.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "bun:test";
-
-import { getHelloWorld } from "./getHelloWorld";
-
-test("ok", () => {
-	const result = getHelloWorld();
-
-	expect(result).toEqual("Hello World");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,48 @@
 #!/usr/bin/env node
-import { getHelloWorld } from "./getHelloWorld";
+
+import { type Result, failure, success } from "composable-functions";
+
+type Command = () => void;
+
+const usageCommand: Command = () => {
+	console.log(`Usage: <command> [options]
+
+Commands:
+	loans	List all loans
+	usage	Display this usage information
+`);
+};
+
+const loansCommand: Command = () => {
+	console.log("Loans command");
+};
+
+const getCommand = (args: string[]): Result<Command> => {
+	if (args.length > 0) {
+		const firstArg = args[0];
+		switch (firstArg) {
+			case "loans":
+				return success(loansCommand);
+			case "usage":
+				return success(usageCommand);
+			default:
+				return failure([new Error(`unknown command: ${firstArg}`)]);
+		}
+	}
+	return failure([new Error("missing command")]);
+};
 
 const main = (): void => {
-	console.log(getHelloWorld());
+	const result = getCommand(process.argv.slice(2));
+	if (result.success) {
+		result.data();
+	} else {
+		for (const error of result.errors) {
+			console.error(error.message);
+		}
+		console.log(); // blank lines
+		usageCommand();
+	}
 };
 
 main();
