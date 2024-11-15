b00fb240d3ec8d25832ae590b3454bce23c8e5ee
Thomas Queste <tom@tomsquest.com>
2024-11-15 06:41:10 +0100

    refactor: extract commands

---
+++ src/commands.ts
@@ -0,0 +1,95 @@
+import { promises as fs } from "node:fs";
+import { sep } from "node:path";
+import { type Result, pipe, success } from "composable-functions";
+
+export type Command = {
+	type: string;
+	run: () => Promise<Result>;
+};
+
+const commands = (errorMessage?: string) => async (): Promise<Result> => {
+	if (errorMessage) {
+		console.error(errorMessage);
+		console.error(); // blank line
+	}
+	console.log("Usage: npx mediathequeroubaix <command>");
+	console.log("Commands:");
+	console.log("  loans: List all loans");
+	console.log("  usage: Show this usage information");
+	return success(undefined);
+};
+
+const loanCommand = async (): Promise<Result> => {
+	console.error("Not yet implemented");
+	return success(undefined);
+};
+
+const getHomeDir = (env: Record<string, string | undefined>): string => {
+	const home = env.HOME;
+	if (home) return home;
+	throw new Error("unable to find home directory");
+};
+
+const getConfigFilename = (homeDir: string): string =>
+	`${homeDir}${sep}.config${sep}mediathequeroubaix${sep}config.json`;
+
+const fileExist = async (file: string): Promise<string> => {
+	try {
+		await fs.access(file, fs.constants.R_OK);
+		return file;
+	} catch (e: unknown) {
+		throw new Error(`file '${file}' is not readable: ${e}`);
+	}
+};
+
+const readFile = (filename: string): Promise<string> =>
+	fs.readFile(filename, "utf-8");
+
+const parseJson = (text: string): Promise<object> => {
+	try {
+		return JSON.parse(text);
+	} catch (e: unknown) {
+		throw new Error(`Unable to parse JSON: ${text}. Error: ${e}`);
+	}
+};
+
+const printConfig = (config: object): Promise<void> => {
+	console.log(JSON.stringify(config, null, 2));
+	return Promise.resolve();
+};
+
+const showConfigCommand = async (): Promise<Result> => {
+	const showConfig = pipe(
+		getHomeDir,
+		getConfigFilename,
+		fileExist,
+		readFile,
+		parseJson,
+		printConfig,
+	);
+	return showConfig(process.env);
+};
+
+export const getCommand = (args: string[]): Command => {
+	if (args.length > 0) {
+		const firstArg = args[0];
+		switch (firstArg) {
+			case "usage":
+				return { type: "usage", run: commands() };
+			case "loans":
+				return { type: "loans", run: loanCommand };
+			case "config":
+				return { type: "config", run: showConfigCommand };
+			default:
+				return {
+					type: "usage",
+					run: commands(`Unknown command: ${firstArg}`),
+				};
+		}
+	}
+
+	return {
+		type: "usage",
+		run: commands("Missing argument, none provided"),
+	};
+};
